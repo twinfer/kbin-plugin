f658159ca914ffc3c2f4c06b1ca4732a3bd64569
khaliddaoud <mohamed.khalid@gmail.com>
2025-05-25 13:53:09 +0300

    clean up

--- test/generate_tests.go
+++
@@ -1,3 +0,0 @@
-package test
-
-//go:generate go run ../scripts/kaitai-test-gen-simple.go  -ksy_dir=test/format -bin_dir=test/src -ksc_gen_dir=testdata/formats_kaitai_go_gen -test_output_dir=testdata/kaitaistruct/formats_test
